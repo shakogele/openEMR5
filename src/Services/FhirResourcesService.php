7bf8fff0aa09b87e3af2898b9aaef173e96e70dc
Shalva Gelenidze <shakogele@gmail.com>
2019-09-20 16:55:49 +0400

    FHIR

--- src/Services/FhirResourcesService.php
+++ src/Services/FhirResourcesService.php
@@ -150,7 +150,7 @@ class FhirResourcesService
       $observationResource = new FHIRObservation($observationResourceData);
       $status = $observationResource->getStatus();
       $identifier = $observationResource->getIdentifier();
-      return $identifier[0];
+      return $identifier[0]["value"];
       $observationArr = [
         "id"          => "5",
         "date"        => "2019-09-20",
