0381728848788eda5ce16bb05bdde5c16cf8340d
Shalva Gelenidze <shakogele@gmail.com>
2019-09-20 14:50:16 +0400

    OBS

--- _rest_routes.inc.php
+++ _rest_routes.inc.php
@@ -374,7 +374,6 @@ RestConfig::$FHIR_ROUTE_MAP = array(
     },
     "GET /fhir/Observation/:oid" => function ($oid) {
         RestConfig::authorization_check("encounters", "auth_a");
-        return ["Hello"=>"there"];
         return (new FhirObservationRestController())->getOne($oid);
     },
     "POST /fhir/Patient" => function () {
